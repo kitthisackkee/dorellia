4e7d3d2a75b2d56affbadf12a5d5901a1f9007e3
kitthisackkee <keomixayhuk@gmail.com>
2022-10-20 08:20:04 +0700

    general layout / wip

---
+++ client/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="p-3">
+      <Logo />
+      <TopMenu />
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import TopMenu from '~/components/TopMenu.vue'
+export default {
+  layout: 'homepage',
+  components: { TopMenu },
+}
+</script>
---
+++ client/pages/yourorder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-12">
+        <h1>Your Order</h1>
+        <p>
+            ....
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'homepage',
+  name: 'your order',
+}
+</script>
